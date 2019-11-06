f7c4e04161f01e792848bbb4e47444dd49c98fbf
AnnaAleksovna <53649649+AnnaAleksovna@users.noreply.github.com>
2019-11-06 09:56:48 +0200

    Printing a document

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿// меняем название страницы
+document.querySelector('title').innerText = 'Print';
+
+// заголовок
+document.querySelector('h1').innerText = '"Киевский торт"';
+
+//кнопка
+/*
+let bt1 = document.querySelector('#bt1');
+bt1.onclick = style2;
+
+function style2 ()
+{
+document.querySelector('link').href = 'style2.css';
+};*/
+
+bt1.onclick = function ()
+{
+	document.querySelector('link').href = 'style2.css';
+};
